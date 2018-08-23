01241e8655954aec318f7c6f28d9abb4e3ea6e23
Matt Lambie <mlambie@lambie.org>
2018-08-23 08:09:41 +0000

    Move to using PHP's curl methods

--- index.php
+++ index.php
@@ -7,9 +7,16 @@ require('token.php');
 // default to Singapore if there's no country code
 $country = !empty($_GET['country']) ? $_GET['country'] : 'sg';
 
-$data = array("status_text" => microtime(), "status_emoji" => ":flag-$country:");                                                                    
-$profile = urlencode(json_encode($data));
+$url 		 = 'https://slack.com/api/users.profile.set';
+$status  = microtime();
+$emoji 	 = ":flag-$country:";
+$profile = urlencode(json_encode(array("status_text" => $status, "status_emoji" => $emoji)));
+$payload = "token=$token&profile=$profile";
 
-$cmd = "curl https://slack.com/api/users.profile.set --data 'token=$token&profile=$profile'";
+$c = curl_init($url);
+curl_setopt($c, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true); // capture the response instead of echoing it
+curl_setopt($c, CURLOPT_POSTFIELDS, $payload);
 
-`$cmd`;
+// capture the response (RETURNTRANSFER)
+$result = curl_exec($c);
